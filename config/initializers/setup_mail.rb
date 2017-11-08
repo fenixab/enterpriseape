d8c15746986b0e6e013d9d7335b204cac309e5bc
fenixab <fenixab1981@gmail.com>
2017-11-08 21:54:42 +0000

    test

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
 	:address			  => 'smtp.sendgrid.net',
-	:port 				  =>  587,
+	:port 				  =>   587,
 	:authentication 	  => :plain,
 	:user_name			  => 'app80285831@heroku.com',
 	:password			  => '2n6xfntz8906',
