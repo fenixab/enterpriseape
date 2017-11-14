943348fecb8e71fad0455ea279fe857a2cafe51b
fenixab <fenixab1981@gmail.com>
2017-11-14 10:51:19 +0000

    added the selectio for paid or pending

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171110120009) do
+ActiveRecord::Schema.define(version: 20171114104127) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20171110120009) do
     t.string   "salesperson"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "status_type"
   end
 
   create_table "users", force: :cascade do |t|
