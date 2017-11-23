0a302e037416098999645c158a367a88d9dad78e
fenixab <fenixab1981@gmail.com>
2017-11-23 14:02:42 +0000

    Added price and chage the currency to euro

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171122131356) do
+ActiveRecord::Schema.define(version: 20171123133434) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20171122131356) do
     t.integer  "invoice_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal  "price"
     t.index ["invoice_id"], name: "index_purchases_on_invoice_id"
   end
 
