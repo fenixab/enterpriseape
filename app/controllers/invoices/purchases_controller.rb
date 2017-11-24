04ee7e28cafe6a1b5cb4760f9b7443b834865160
fenixab <fenixab1981@gmail.com>
2017-11-24 10:09:46 +0000

    finished edit in the purchase controller

--- app/controllers/invoices/purchases_controller.rb
+++ app/controllers/invoices/purchases_controller.rb
@@ -1,39 +1,27 @@
 class Invoices::PurchasesController < ApplicationController
     before_action :set_invoice
+    before_action :set_purchase, except: [:new, :create] 
     
-
-  # GET /purchases
-  # GET /purchases.json
-  def index
-    @purchases = Purchase.all
-  end
-
-  # GET /purchases/1
-  # GET /purchases/1.json
-  def show
-  end
 
   # GET /purchases/new
   def new
-    @invoice = Invoice.find(params[:invoice_id])
+    
     @purchase = Purchase.new
   end
 
   # GET /purchases/1/edit
   def edit
-     
   end
 
   # POST /purchases
   # POST /purchases.json
   def create
-    @invoice = Invoice.find(params[:invoice_id])
     @purchase = Purchase.new(purchase_params)
     @purchase.invoice = @invoice
 
     respond_to do |format|
       if @purchase.save
-        format.html { redirect_to @invoice, notice: 'Purchase was successfully created.' }
+        format.html { redirect_to @invoice, notice: 'The Purchase was successfully created.' }
         format.json { render :show, status: :created, location: @invoice }
       else
         format.html { render :new }
@@ -47,19 +35,18 @@ class Invoices::PurchasesController < ApplicationController
   def update
     respond_to do |format|
       if @purchase.update(purchase_params)
-        format.html { redirect_to @purchase, notice: 'Purchase was successfully updated.'}
+        format.html { redirect_to @invoice, notice: 'The Purchase was successfully updated.'}
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
-        format.json { render json: @purchase.errors, status: :unprocessable_entity}
+        format.json { render json: @invoice.errors, status: :unprocessable_entity}
+      end
     end
   end
 
   # DELETE /purchases/1
   # DELETE /purchases/1.json
   def destroy
-    @invoice = Invoice.find(params[:invoice_id])
-    @purchase = Purchase.find(params[:id])
     title = @purchase.name
 
         if @purchase.destroy
@@ -77,6 +64,10 @@ class Invoices::PurchasesController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.  
     def set_invoice
       @invoice = Invoice.find(params[:invoice_id])
+    end
+
+    def set_purchase
+      @purchase = Purchase.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
